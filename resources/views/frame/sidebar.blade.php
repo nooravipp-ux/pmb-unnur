a33f30ee47a48238b3b64cb8f8db596cd6609eac
nooravipp-ux <54164824+nooravipp-ux@users.noreply.github.com>
2020-09-01 12:36:13 +0700

    add menu sidebar

--- resources/views/frame/sidebar.blade.php
+++ resources/views/frame/sidebar.blade.php
@@ -17,9 +17,72 @@
         </li>
         @endcan
         @can('manage-prodi')
-        <li class="@yield('title_admin')"><a><i class="fa fa-home"></i> Menu Prodi <span class="fa fa-chevron-down"></span></a>
+        <li class="@yield('title_admin')"><a><i class="fa fa-home"></i>Dashboard dan Lap PMB<span class="fa fa-chevron-down"></span></a>
           <ul class="nav child_menu">
             <li class="@yield('tchild_admin')"><a href="#">Aktivasi Akun PMB</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Laporan Kelulusan Ujian Online</a></li>
+          </ul>
+        </li>
+        <li class="@yield('title_admin')"><a><i class="fa fa-home"></i>Pendaftaran Online<span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li class="@yield('tchild_admin')"><a href="#">Aktivasi Calon Mahasiswa</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Info Registrasi</a></li>
+          </ul>
+        </li>
+        <li class="@yield('title_admin')"><a><i class="fa fa-home"></i>Data Calon Mahasiswa<span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li class="@yield('tchild_admin')"><a href="#">Cari Calon Mahasiswa</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Impor Photo</a></li>
+          </ul>
+        </li>
+        <li class="@yield('title_admin')"><a><i class="fa fa-home"></i>Nilai Ujian<span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li class="@yield('tchild_admin')"><a href="#">Entry Nilai Ujian</a></li>
+          </ul>
+        </li>
+        <li class="@yield('title_admin')"><a><i class="fa fa-home"></i>Proses Kelulusan<span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li class="@yield('tchild_admin')"><a href="#">Jalur Seleksi Ujian</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Jalur Seleksi Non Ujian</a></li>
+          </ul>
+        </li>
+        <li class="@yield('title_admin')"><a><i class="fa fa-home"></i>Biaya Mahasiswa Baru<span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li class="@yield('tchild_admin')"><a href="#">Data Dasar</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Penerimaan Beasiswa</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Potongan Keuangan</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Biaya Komponen per Mahasiwa Baru</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Tagihan</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Info Rencana Pembayaran</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Laporan Pembayaran</a></li>
+          </ul>
+        </li>
+        <li class="@yield('title_admin')"><a><i class="fa fa-home"></i>Registrasi<span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li class="@yield('tchild_admin')"><a href="#">Registrasi Mahasiswa Baru</a></li>
+          </ul>
+        </li>
+        <li class="@yield('title_admin')"><a><i class="fa fa-home"></i>Pengaturan<span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li class="@yield('tchild_admin')"><a href="#">Jadwal Pendaftaran dan Biaya</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Jurusan Asal</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Pass in Grade & Kuota</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Setting Jalur Masuk</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Setting Prodi PMB</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Setting ID PMB Otomatis</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Setting NIM Otomatis</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Jumlah Piliha Prodi</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Metode Penilaian</a></li>
+          </ul>
+        </li>
+        <li class="@yield('title_admin')"><a><i class="fa fa-home"></i>Konfigurasi<span class="fa fa-chevron-down"></span></a>
+          <ul class="nav child_menu">
+            <li class="@yield('tchild_admin')"><a href="#">Konfigurasi Portal PMB</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Konfigurasi Mail/SMTP</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Konfigurasi Pendaftaran</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Konfigurasi Reset Password</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Konfigurasi Pengunguman</a></li>
+            <li class="@yield('tchild_admin')"><a href="#">Keterangan</a></li>
           </ul>
         </li>
         @endcan
