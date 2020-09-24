691f7699300224c643d3096bf7abcacff163235a
Hafiiidz <hafidzp719@gmail.com>
2020-09-24 12:31:41 +0700

    fixxx
    fixxx

--- resources/views/setting_pmb/biaya_registrasi.blade.php
+++ resources/views/setting_pmb/biaya_registrasi.blade.php
@@ -13,18 +13,9 @@
             <div class="col-md-12 col-sm-12 ">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Biaya Registrasi PMB</h2>
+                    <h2>Form | Biaya Registrasi PMB</h2>
                     <ul class="nav navbar-right panel_toolbox">
                       <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Settings 1</a>
-                            <a class="dropdown-item" href="#">Settings 2</a>
-                          </div>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
                       </li>
                     </ul>
                     <div class="clearfix"></div>
@@ -89,18 +80,9 @@
             <div class="col-md-12 col-sm-12 ">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Data Biaya Registrasi PMB</h2>
+                    <h2>Form | Data Biaya Registrasi PMB</h2>
                     <ul class="nav navbar-right panel_toolbox">
                       <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Settings 1</a>
-                            <a class="dropdown-item" href="#">Settings 2</a>
-                          </div>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
                       </li>
                     </ul>
                     <div class="clearfix"></div>
@@ -217,7 +199,7 @@
         });
 
         $('#id_jenjang_prodi').select2({
-    		placeholder: '- Pilih Jenjang Podi -',
+    		placeholder: '- Pilih Jenjang Prodi -',
             ajax: {
                 url:  '{{url('/prodi/get_data_jenjang_prodi')}}',
                 dataType: 'json',
