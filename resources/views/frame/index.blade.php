ca8c2a03a1245e33b191dbc11e0a097111cd96cb
sandityasn10 <45733733+sandityasn10@users.noreply.github.com>
2020-09-28 13:29:02 +0700

    add favicon
    favicon

--- resources/views/frame/index.blade.php
+++ resources/views/frame/index.blade.php
@@ -7,7 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-	<link rel="icon" href="images/favicon.ico" type="image/ico" />
+	<!-- Favicons -->
+    <link href="{{ asset('lp/assets/images/favicon.png') }}" rel="icon">
 
 
     <title>PMB | UNNUR</title>
